083040777839286949e651eb7073e240d9afc726
Shreyash Shukla <shreyashshukla194@gmail.com>
2026-02-18 01:39:25 +0530

    Add even/odd checker in even_Odd.java

---
+++ Basics/even_Odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
+}
