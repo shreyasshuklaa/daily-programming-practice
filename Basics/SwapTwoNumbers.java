bf89364a7076fb7011b1075a0f72bb2d53f570e2
Shreyash Shukla <shreyashshukla194@gmail.com>
2026-02-18 01:45:17 +0530

    Add SwapTwoNumbers program to swap two integers

---
+++ Basics/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+
+        input.close();
+    }
+}
