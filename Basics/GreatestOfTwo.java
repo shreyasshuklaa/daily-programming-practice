c81f743164fd2789fa6aee98baf08baf597f6d43
Shreyash Shukla <shreyashshukla194@gmail.com>
2026-02-18 01:43:08 +0530

    Add GreatestOfTwo program to find the greater number

---
+++ Basics/GreatestOfTwo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GreatestOfTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+
+        input.close();
+    }
+}
